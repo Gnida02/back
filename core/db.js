da0e3878a2f9192cad8c573f05e3b86da9a4332b
klrushka <comeindonotbeshy@gmail.com>
2024-05-17 13:29:52 +0300

    db

--- core/db.js
+++ core/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mongoUrl = process.env.MONGO_URL
 
 mongoose
-  .connect('mongodb://0.0.0.0:27017/dental', {
+  .connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
